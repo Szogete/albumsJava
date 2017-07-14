bfc4f9230305d50914e0dcbddb19e735f2eaab83
Lukasz-PC\LukaszDell <pigi92@gmail.com>
2017-07-14 21:46:11 +0200

    hibernate inital

--- src/Album.java
+++ src/Album.java
@@ -6,19 +6,60 @@ import java.util.List;
 public class Album {
     public String title = "";
     public String artist = "";
-    public int duration = 0;
+    public double duration = 0;
     public ArrayList<Track> tracks;
+    public int albumId = 0;    
   
-
-    Album(String title, String artist, int duration, ArrayList<Track> tracks) {
+    Album()
+    {
+    }
+    
+    Album(String title, String artist, double duration, ArrayList<Track> tracks) {
         this.title = title;
         this.artist = artist;
         this.duration = duration;
         this.tracks = tracks;
     }
     
+    public String getTitle() 
+    {  
+        return title;  
+    }  
     
+    public void setTitle(String title) 
+    {  
+        this.title = title;  
+    }  
+    
+    public String getArtist() 
+    {  
+        return artist;  
+    }  
+    
+    public void setArtist(String artist) 
+    {  
+        this.artist = artist;  
+    }  
         
+    public double getDuration() 
+    {  
+        return duration;  
+    }  
+    
+    public void setDuration(double duration) 
+    {  
+        this.duration = duration;  
+    }  
+    
+    public double getAlbumId() 
+    {  
+        return albumId;  
+    }  
+    
+    public void setAlbumId(int AlbumId) 
+    {  
+        this.albumId = albumId;  
+    }  
    @Override        //żeby ładniej wyglądało
    public String toString() { 
     return this.title;
--- src/Track.java
+++ src/Track.java
@@ -1,8 +1,54 @@
 public class Track {
 
-    public final String trackName;
-    public final double duration;
+    public String trackName;
+    public double duration;
+    public int albumId = 0;
+    public int trackId = 0;
       
+    Track()
+    {
+    }
+    
+     public String getTrackName() 
+    {  
+        return trackName;  
+    }  
+    
+    public void setTrackName(String TrackName) 
+    {  
+        this.trackName = trackName;  
+    }  
+    
+    public double getDuration() 
+    {  
+        return duration;  
+    }  
+    
+    public void setDuration(double duration) 
+    {  
+        this.duration = duration;  
+    }  
+    
+    public double getAlbumId() 
+    {  
+        return albumId;  
+    }  
+    
+    public void setAlbumId(int AlbumId) 
+    {  
+        this.albumId = albumId;  
+    }  
+    
+    public double getTrackId() 
+    {  
+        return trackId;  
+    }  
+    
+    public void setTrackId(int TrackId) 
+    {  
+        this.trackId = trackId;  
+    }  
+    
     Track(String trackName, double duration) {
         this.trackName = trackName; 
         this.duration = duration; 
